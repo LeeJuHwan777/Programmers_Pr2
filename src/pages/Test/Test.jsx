804895a1d0539d817847415318df7f6e39581ab3
이주환 <wnghks145@gmail.com>
2024-10-02 18:20:25 +0900

    [Test4] 2-1

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,11 +1,9 @@
 import PostingSwiper from "../../components/common/Swiper/Posting";
-import PostingCard from "../../components/features/Posting/PostingCard";
 
 export default function Test() {
   return (
     <>
       <PostingSwiper />
-      <PostingCard />
     </>
   );
 }
