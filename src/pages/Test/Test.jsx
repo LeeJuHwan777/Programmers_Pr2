cc144b21b01d5e59e6e899f84c47c63605334068
이주환 <wnghks145@gmail.com>
2024-10-02 18:17:04 +0900

    [Test4] Test

---
+++ src/pages/Test/Test.jsx
@@ -0,0 +1,11 @@
+import PostingSwiper from "../../components/common/Swiper/Posting";
+import PostingCard from "../../components/features/Posting/PostingCard";
+
+export default function Test() {
+  return (
+    <>
+      <PostingSwiper />
+      <PostingCard />
+    </>
+  );
+}
