6dd618c21908b5a66e33fa98a3c87fe57e1e3961
이주환 <wnghks145@gmail.com>
2024-10-04 11:21:59 +0900

    [Feat] Posting Navigation 추가

---
+++ src/components/common/Navigation/index.jsx
@@ -0,0 +1,8 @@
+export default function NewsNavigation() {
+  return (
+    <div className="custom-navigation">
+      <button className="swiper-button-prev"><img src="/News/Blog/prev.png" alt="이전" /></button>
+      <button className="swiper-button-next"><img src="/News/Blog/next.png" alt="다음" /></button>
+    </div>
+  );
+}
--- src/components/common/Swiper/Posting/index.jsx
+++ src/components/common/Swiper/Posting/index.jsx
@@ -3,23 +3,36 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
+import "swiper/css";
 import PostingCard from "../../../features/Posting/PostingCard";
 
 // import required modules
-
+// import { Navigation } from "swiper/modules";
+import NewsNavigation from "../../Navigation";
 
 export default function PostingSwiper() {
   return (
-      <Swiper slidesPerView={4}
-      spaceBetween={30}
-      >
-      <SwiperSlide><PostingCard /></SwiperSlide>
-      <SwiperSlide><PostingCard /></SwiperSlide>
-      <SwiperSlide><PostingCard /></SwiperSlide>
-      <SwiperSlide><PostingCard /></SwiperSlide>
-      <SwiperSlide><PostingCard /></SwiperSlide>
-      <SwiperSlide><PostingCard /></SwiperSlide>
-      </Swiper>
+  <Swiper
+    slidesPerView={4}
+    spaceBetween={30}
+    pagination={{
+      clickable: true,
+    }}
+    navigation={{
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }} // 커스텀 네비게이션 설정
+  >
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <SwiperSlide><PostingCard /></SwiperSlide>
+    <NewsNavigation />
+  </Swiper>
   );
 }
