40c97a865a79ced5336f7e12f09f8e1b8f3a68b3
이주환 <wnghks145@gmail.com>
2024-10-04 11:47:33 +0900

    [Style] 인기포스팅 emotion css 적용

--- src/components/common/Navigation/index.jsx
+++ src/components/common/Navigation/index.jsx
@@ -1,8 +1,22 @@
+import styled from "@emotion/styled";
+
 export default function NewsNavigation() {
   return (
-    <div className="custom-navigation">
-      <button className="swiper-button-prev"><img src="/News/Blog/prev.png" alt="이전" /></button>
-      <button className="swiper-button-next"><img src="/News/Blog/next.png" alt="다음" /></button>
-    </div>
+    <CustomNavigation>
+      <NewsNavigationWrapper className="swiper-button-prev">
+        <img src="/News/Blog/prev.png" alt="이전" />
+      </NewsNavigationWrapper>
+      <NewsNavigationWrapper className="swiper-button-next">
+        <img src="/News/Blog/next.png" alt="다음" />
+      </NewsNavigationWrapper>
+    </CustomNavigation>
   );
 }
+const NewsNavigationWrapper = styled.button`
+  border: none;
+  background-color: var(--main);
+  margin: 38px 75px 0 0;
+  cursor: pointer;
+`;
+
+const CustomNavigation = styled.div``;
